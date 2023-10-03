43927546c888f28d70288f03ea6eec02d701f2d6
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-03 20:27:18 +0530

    Added a React Project named 'Textutils'

---
+++ textutils/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <>
+<Navbar title="TextUtils" aboutText="About Us" />
+    </>
+  );
+}
+
+export default App;
