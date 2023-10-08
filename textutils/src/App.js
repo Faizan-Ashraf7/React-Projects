4b5dd061e2d842a2a635a7c5d0ecb90450678bc5
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-08 23:44:34 +0530

    added header

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 function App() {
   return (
     <>
 <Navbar title="TextUtils" aboutText="About Us" />
+<Header/>
     </>
   );
 }
