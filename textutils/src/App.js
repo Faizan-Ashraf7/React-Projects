a7b9388255764f64117a126b012455ad19c103b7
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-09 21:58:12 +0530

    Added container

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -4,8 +4,10 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-<Navbar title="TextUtils" aboutText="About Us" />
-<TextForm />
+  <Navbar title="TextUtils" aboutText="About Us" />
+  <div className="container">
+  <TextForm />
+  </div>
     </>
   );
 }
