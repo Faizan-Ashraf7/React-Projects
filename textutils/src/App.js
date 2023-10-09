5717a30f7a9367822596714a3be1d7751b7ed2b4
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-09 21:55:21 +0530

    Imported components

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import TextForm from "./components/TextForm";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
 <Navbar title="TextUtils" aboutText="About Us" />
+<TextForm />
     </>
   );
 }
