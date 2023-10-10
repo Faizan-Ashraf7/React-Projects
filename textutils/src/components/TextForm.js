35fd944b8ad20be978cd17657d084c641c7911da
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-10 18:40:26 +0530

    Added JSX elements

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -5,18 +5,18 @@ export default function TextForm(props) {
     <div>
       <h1>{props.heading}</h1>
       <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
   </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
   </div>
-  <div class="mb-3 form-check">
-    <label class="form-label" for="myBox"></label>
-    <textarea  class="form-control" id="myBox" rows ="8"/>
+  <div className="mb-3 form-check">
+    <label className="form-label" htmlFor="myBox"></label>
+    <textarea  className="form-control" id="myBox" rows ="8"/>
   </div>
   <button className="btn btn-primary">Convert to upper case</button>
 </form>
