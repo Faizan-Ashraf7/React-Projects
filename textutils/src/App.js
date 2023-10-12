c8f7c237e5f08a2009e2687b9201f6e1423f9510
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-12 23:51:59 +0530

    Added element

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-    <div>
+    <div className="Navbar">
   <Navbar title="TextUtils" aboutText="About Us" />
   </div>
   <div className="container my-3">
