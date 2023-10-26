e8212d4b57d51af4c667a3c9141a214ff63decc9
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-26 21:10:28 +0530

    Adding new functionalities

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -8,10 +8,9 @@ function App() {
   <Navbar title="TextUtils" aboutText="About Us" />
   </div>
   <div className="container my-3">
-  <TextForm heading="Enter the form data below"/>
+  <TextForm heading="Enter the text below to analyze"/>
   </div>
   <div>
-
   </div>
     </>
   );
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -7,31 +7,21 @@ export default function TextForm(props) {
   setText(text.toUpperCase());
   };
   const handleOnChange=(event)=>{
-  console.log(`On change`);
   setText(event.target.value);
   };
   // text=`new text`; //wrong way
   // setText(`new text`); // correct way
   return (
-    <div>
+    <>
+      <div>
       <h1>{props.heading} </h1>
-      <form>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"/>
-  </div>
-  <div className="mb-3 form-check">
-    <label className="form-label" htmlFor="myBox"></label>
+      
     <textarea  className="form-control" id="myBox" value={text} onChange={handleOnChange} rows ="8"/>
-  </div>
-  <button className="btn btn-primary" onClick={handleUpClick}>Convert to upper case</button>
-</form>
     </div>
-    
+  <div>
+  <button className="btn btn-primary" onClick={handleUpClick}>Convert to upper case</button>
+
+    </div>
+    </>
   )
 }
