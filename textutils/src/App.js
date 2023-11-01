6087755834cd77eb0d0be5e20a68b631ce66eeb3
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-11-01 20:26:29 +0530

    Making dark mode for the whole app

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import TextForm from "./components/TextForm";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import { useState } from "react";
 function App() {
+  const [darkmode,setDarkMode]= useState(false); // Whether dark mode is enabled or not
+
   return (
     <>
     <div className="Navbar">
-  <Navbar title="TextUtils" aboutText="About Us" />
+  <Navbar title="TextUtils" mode={darkmode}/>
   </div>
   <div className="container my-3">
   <TextForm heading="Enter the text below to analyze"/>
